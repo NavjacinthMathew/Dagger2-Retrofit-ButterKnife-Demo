3f7f27150f5d250dcd1dc2ff9b01299eb9934e22
Navjacinth Mathew <jacinth9@gmail.com>
2017-01-26 05:47:33 +0530

    Added Project

---
+++ app/src/main/java/mathew/navjacinth/com/dagger2retrofitdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package mathew.navjacinth.com.dagger2retrofitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
