cbcf9635ed4938c25e56d8400dba5f85c04898f7
Navjacinth Mathew <jacinth9@gmail.com>
2017-01-30 09:27:44 +0530

    -Added MyApplication

---
+++ app/src/main/java/mathew/navjacinth/com/dagger2retrofitdemo/misc/MyApplication.java
@@ -0,0 +1,14 @@
+package mathew.navjacinth.com.dagger2retrofitdemo.misc;
+
+import android.app.Application;
+
+/**
+ * Created by Navjacinth Mathew on 1/29/2017.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
